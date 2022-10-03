4e0fa9cdee5cd6384e7377bb3661f978ca02e82a
Karter <kettlechips12@gmail.com>
2022-10-03 11:49:56 -0400

    8ball

---
+++ 8ball.py
@@ -0,0 +1,28 @@
+name = "joe"
+
+question = "Am I gay?"
+
+answer = ""
+import random 
+random_number = random.radiant(1,9)
+print(random_number)
+if random_number == 1:
+  answer = "yes - defintely"
+elif random_number == 2:
+  awnser ="It is decidedly so."
+elif random_number == 3:
+  awnser ="Without a doubt"
+elif random_number == 4:
+  awnser ="Reply hazy, try again."
+elif random_number == 5:
+  awnser ="Ask again later"
+elif random_number == 6:
+  awnser ="Better not tell"
+elif random_number == 7:
+  awnser ="My source code tells me no"
+elif random_number == 8:
+  awnser ="Outlook not so great"
+elif random_number == 9:
+  awnser ="verry doubtful, sorry for your luck"
+else:
+  anser = "Error"
